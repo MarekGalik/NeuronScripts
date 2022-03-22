def37250e15a0385f2050ef2b734636f65fecbdd
marek_galik <galik.marekk@gmail.com>
2022-03-22 20:03:32 +0100

    MergeData.py works

--- MergeData.py
+++ MergeData.py
@@ -7,19 +7,19 @@ numOfSources = len(sys.argv) - 2
 tabulka = pd.DataFrame(columns=["Dir","Name"])
 targetPath = sys.argv[len(sys.argv) - 1]
 counter = 0
+f = open(os.path.join(targetPath, "tabulka.txt"),"w+")
 
 for i in range(1, numOfSources + 1):
     sourcePath = sys.argv[i]
     listDir = os.listdir(sourcePath)
-    df = pd.read_csv(os.path.join(sourcePath, "tabulka.txt"), header=[0,1])
-    
+    df = pd.read_csv(os.path.join(sourcePath, "tabulka.txt"), names=[0,1], sep=' ')
+
     numofDirs = len(listDir)
     for j in range(numofDirs - 1):
         counter += 1
-        nameOfFolder = listDir[j]
-        #rowIndex = df.index[df[0] == nameOfFolder]
-        #rowIndex = pd.Index[df[0] == nameOfFolder]
-        name = df[int(df[0] == nameOfFolder), 1]
-        shutil.copy(os.path.join(sourcePath, nameOfFolder), targetPath)
-        os.rename(os.path.join(targetPath, nameOfFolder), os.path.join(targetPath, str(counter)))  
-        tabulka = tabulka.append({'Dir' : str(j + 1), 'Name' : name})
+        index = listDir.index((str(j + 1)))
+        nameOfFolder = listDir[index]
+        name = str(df.loc[j, 1])
+        shutil.copytree(os.path.join(sourcePath, nameOfFolder), os.path.join(targetPath, str(counter)))  
+        f.write(str(counter) + " " + name + "\n")
+        
